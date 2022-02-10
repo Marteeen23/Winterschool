0afd06508a8e37a54c0be66d2c778a4a9a7cc2e8
Ayush Deep <ayushdeep2001@gmail.com>
2022-02-10 13:06:42 +0530

    Minor Changes

---
+++ JS/contact-us.js
@@ -0,0 +1,13 @@
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyg-uW0LQBcGJ-d-WK8N38Dx_OdufceXp-n7NFe9rfoL0gCP-Xf7hctewTJYBXLJ2FT/exec'
+    const form = document.forms['google-sheet']
+
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      fetch(scriptURL, {
+          method: 'POST',
+          body: new FormData(form)
+        })
+        .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+        .catch(error => console.error('Error!', error.message))
+    })
