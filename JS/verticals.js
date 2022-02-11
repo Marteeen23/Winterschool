588ef5b6e1ad24f287b1afa91a694439017580f8
Ayush Deep <ayushdeep2001@gmail.com>
2022-02-12 03:30:12 +0530

    Schedule Page

---
+++ JS/verticals.js
@@ -0,0 +1,39 @@
+function openDay(evt, dayName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+  
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].className = tabcontent[i].className.replace(" activeDay", "");
+	}
+  
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	// Show the current tab, and add an "active" class to the link that opened the tab
+	var el = document.getElementById(dayName);
+	el.className += " activeDay"
+	evt.currentTarget.className += " active";
+  };
+
+function activateCV() {
+    document.getElementById("vertical-cv").style.display = "block";
+    document.getElementById("vertical-auto").style.display = "none";
+    document.getElementById("vertical-ml").style.display = "none";
+};
+
+function activateAuto() {
+    document.getElementById("vertical-cv").style.display = "none";
+    document.getElementById("vertical-auto").style.display = "block";
+    document.getElementById("vertical-ml").style.display = "none";
+};
+
+function activateML() {
+    document.getElementById("vertical-cv").style.display = "none";
+    document.getElementById("vertical-auto").style.display = "none";
+    document.getElementById("vertical-ml").style.display = "block";
+};
