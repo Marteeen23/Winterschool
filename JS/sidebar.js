e341b6dbc14f2c844ba59eba5fd2ddc498ac9b87
Ayush Deep <ayushdeep2001@gmail.com>
2022-02-13 21:06:06 +0530

    TRS vertical Text Added

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -11,12 +11,14 @@ function toggleSidebar() {
         }
         document.getElementById("mySidebar").style.boxShadow = "0px 0px 10000px 10000px rgba(0,0,0,0.7)";
         document.getElementById("rbtx").style.opacity = "1";
+        // document.getElementById("brand-name-section").style.display = "none";
         this.mini = false;
     } else {
         // console.log("closing sidebar");
         document.getElementById("mySidebar").style.width = "60px";
         document.getElementById("mySidebar").style.boxShadow = "none";
         document.getElementById("rbtx").style.opacity = "0";
+        // document.getElementById("brand-name-section").style.display = "block";
         this.mini = true;
     }
 }
